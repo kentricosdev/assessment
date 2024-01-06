0b3ff913cf12c9a30410dff874243ec9f0dcb090
kentricosdev <assessmentlight@gmail.com>
2024-01-06 12:14:51 -0300

    finished links II

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -50,7 +50,7 @@ const Header: React.FC = () => {
               <Link to="/assessment" onClick={handleStartAssessment}>Xcore</Link>
             </NavigationItem>
             <NavigationItem>
-              <Link to="/#AboutKentricosId" onClick={handleOpenModal}>Quem somos?a</Link>
+              <a href="/#AboutKentricosId" onClick={handleOpenModal}>Quem somos?</a>
             </NavigationItem>
             <NavigationItem>
               <a href="/#comunityId">Comunidade</a>
